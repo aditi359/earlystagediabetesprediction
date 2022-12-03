870973640144d51e3a6d394c0f93f8c2b978eec8
aditi359 <119662414+aditi359@users.noreply.github.com>
2022-12-03 11:50:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,8 +114,7 @@ def run():
             n = 1
         else:
             n = 0
-        link9 = '[CONTACT US](https://aditi359-contactus-contact-form-gkb347.streamlit.app/)'
-        st.markdown(link9, unsafe_allow_html=True)
+        
          
 
         if st.form_submit_button("Submit"):
@@ -133,5 +132,6 @@ def run():
                     "you have early stage diabetic symptoms. you should consult to the doctor."
                     )
 
-
+    link9 = '[CONTACT US](https://aditi359-contactus-contact-form-gkb347.streamlit.app/)'
+        st.markdown(link9, unsafe_allow_html=True)
 run()
