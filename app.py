f9ce46bc73100ad6e44e9989ac431b7896035db4
aditi359 <119662414+aditi359@users.noreply.github.com>
2022-12-03 10:29:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 def run():
     with st.form(key='my_form'):
         st.title("Early Stage diabetes prediction")
-        file_ = open("act4yourheart-diabetes.gif", "rb")
-        contents = file_.read()
-        st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',unsafe_allow_html=True,)
-        data_url = base64.b64encode(contents).decode("utf-8")
-        file_.close()
-        st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',unsafe_allow_html=True,)
+       
         gender = st.radio("Gender: ", ('Male', 'Female'))
         if gender == 'Male':
             a = 1
